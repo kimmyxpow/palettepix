cbf7525e873373629db88ddccc9dc8bb3c41bcf2
= <bbhpowpow@gmail.com>
2021-09-21 11:36:16 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['index.html', 'generate.html']
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'montserrat': '"Montserrat", sans-serif'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
